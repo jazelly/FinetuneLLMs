e83f19106ab5652464d69eb7a545c9365840e074
jazelly <xzha4350@gmail.com>
2024-05-05 19:57:35 +0930

    update submodules

--- mac/slowllama/slowllama/conf_fp16.py
+++
@@ -1,9 +0,0 @@
-import torch
-from conf import *
-
-adamw_eps = 1e-4
-compute_dtype = torch.float16
-frozen_dtype = torch.float16
-
-frozen_model_path = '../llama13b_f16'
-#frozen_model_path = '../llama13b_f16-out'
